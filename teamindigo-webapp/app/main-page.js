c9f88e9c8af0f8ff00d0f2ff36344e51ebffe19a
MarciaSatie <m.satie@gmail.com>
2023-08-22 22:49:59 +0100

    working n progress Main-Page

--- teamindigo-webapp/app/main-page.js
+++ teamindigo-webapp/app/main-page.js
@@ -2,3 +2,19 @@ function openPage() {
   const url = "expense_form.html";
   window.open(url);
 }
+
+//calc and Display Current Budget test.
+
+function calcBudget() {
+  //Getting the value from montlyBusget input
+  let mBudget = document.getElementById("montlyBudget").value;
+  console.log(mBudget);
+
+  //Getting the SUM from current expenses (replace the value for cBudget)
+  let cBudget = 500;
+
+  let result = mBudget - cBudget;
+
+  document.getElementById("currentBudget").innerHTML =
+    "Current Budget: £ " + result;
+}
