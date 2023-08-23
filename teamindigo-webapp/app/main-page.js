2a2657a44d2b1ee8f671b380afa82aa410ed2e4a
MarciaSatie <m.satie@gmail.com>
2023-08-23 21:03:29 +0100

    Using Javascript to fill table information

--- teamindigo-webapp/app/main-page.js
+++ teamindigo-webapp/app/main-page.js
@@ -18,3 +18,66 @@ function calcBudget() {
   document.getElementById("currentBudget").innerHTML =
     "Current Budget: £ " + result;
 }
+
+//Function to fill the table:
+//Placeholder values for testing
+let tableArray = [
+  {
+    expenseName: "New shoes",
+    category: "Personal",
+    subcategory: "clothes",
+    frequency: "Only once",
+    expValue: 60.0,
+    essential: 2,
+    savingDesc: "No",
+    savingValue: 0,
+  },
+  {
+    expenseName: "Netflix",
+    category: "Entertainment",
+    subcategory: "Streaming",
+    frequency: "Montly",
+    expValue: 15.99,
+    essential: 5,
+    savingDesc: "chaging Subscription type",
+    savingValue: 5.99,
+  },
+  {
+    expenseName: "Rent",
+    category: "Housing",
+    subcategory: "Renting",
+    frequency: "Montly",
+    expValue: 850.0,
+    essential: 10,
+    savingDesc: "No",
+    savingValue: 0,
+  },
+];
+
+function fillTable() {
+  let newTr = document.createElement("tr");
+  console.log(newTr);
+
+  let myTable = document.getElementById("tableExpenses");
+  for (i = 0; i <= tableArray.length; i++) {
+    myTable.innerHTML +=
+      "<tr><td>" +
+      tableArray[i].expenseName +
+      "</td><td>" +
+      tableArray[i].category +
+      "</td><td>" +
+      tableArray[i].subcategory +
+      "</td><td>" +
+      tableArray[i].frequency +
+      "</td><td>" +
+      tableArray[i].expValue +
+      "</td><td>" +
+      tableArray[i].essential +
+      "</td><td>" +
+      tableArray[i].savingDesc +
+      "</td><td>" +
+      tableArray[i].savingValue +
+      "</td></tr>";
+  }
+}
+fillTable();
