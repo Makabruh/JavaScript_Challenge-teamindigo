1b186dd4961f0efe00ba15333be5188c78e04610
Makabruh <milly@roach.me.uk>
2023-08-25 23:28:48 +0100

    Added missing back

--- teamindigo-webapp/app/add-expense.js
+++ teamindigo-webapp/app/add-expense.js
@@ -52,13 +52,60 @@ async function createExpenseRequest() {
     document.getElementById('savingValue').textContent = jsonResponse.savingValue;
 }
 
-//Might not need this function - same here
+//Might not need this function
 function createExpense() {
-    document.getElementById('expenseName').value = document.getElementById('expenseName').textContent;
-    document.getElementById('category').value = document.getElementById('category').textContent;
-    document.getElementById('frequency').value = document.getElementById('frequency').textContent;
-    document.getElementById('expValue').value = document.getElementById('expValue').textContent;
-    //missing here
-    document.getElementById('savingDesc').value = document.getElementById('savingDesc').textContent;
-    document.getElementById('savingValue').value = document.getElementById('savingValue').textContent;
+  document.getElementById("expenseName").value =
+    document.getElementById("expenseName").textContent;
+  document.getElementById("category").value =
+    document.getElementById("category").textContent;
+  document.getElementById("frequency").value =
+    document.getElementById("frequency").textContent;
+  document.getElementById("expValue").value =
+    document.getElementById("expValue").textContent;
+  //missing here
+  document.getElementById("savingDesc").value =
+    document.getElementById("savingDesc").textContent;
+  document.getElementById("savingValue").value =
+    document.getElementById("savingValue").textContent;
+}
+
+//Function to get the value of checked Radio Button Frequency
+
+// varible that will be storage the Radio value for Frequency
+let frequencyValue = "";
+function displayRadioValue(radioName) {
+  let selectedValue = "";
+  let radioOption = document.getElementsByName(radioName);
+  console.log("calling function " + radioOption);
+
+  for (i = 0; i < radioOption.length; i++) {
+    console.log(radioOption[i]);
+
+    if (radioOption[i].checked) {
+      selectedValue = radioOption[i].value;
+      console.log("Selected value:", selectedValue);
+      break; // Exit the loop since we found the checked radio button
+    }
+  }
+  frequencyValue = selectedValue;
+}
+
+// varible that will be storage the Radio value for Essential
+let essentialValue = 0;
+
+function displayRadioValue2(radioName) {
+  let selectedValue = "";
+  let radioOption = document.getElementsByName(radioName);
+  console.log("calling function " + radioOption);
+
+  for (i = 0; i < radioOption.length; i++) {
+    console.log(radioOption[i]);
+
+    if (radioOption[i].checked) {
+      selectedValue = radioOption[i].value;
+      console.log("Selected value:", selectedValue);
+      break; // Exit the loop since we found the checked radio button
+    }
+  }
+  essentialValue = selectedValue;
 }
